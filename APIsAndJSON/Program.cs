6166ee222b2810e4bc7c3605b43ecba0506749a6
eopong87 <eopong87@gmail.com>
2023-07-21 15:42:49 -0400

    RonandYe

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            for (int count = 0; count <= 5; count++)
+
+            {
+                HttpClient client = new HttpClient();
+
+                var yeURL = "https://api.kanye.rest";
+
+                var yeResponse = client.GetStringAsync(yeURL).Result;
+
+                var yeObject = JObject.Parse(yeResponse);
+
+                Console.WriteLine($"kayne says: {yeObject["quote"]}");
+
+
+                HttpClient client1 = new HttpClient();
+
+                var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+                var ronResponse = client1.GetStringAsync(ronURL).Result;
+
+                var ronObject = JArray.Parse(ronResponse);
+
+                Console.WriteLine($"Ron says: {ronObject[0]}");
+
+                Console.WriteLine(ronObject[0]);
+
+                Console.WriteLine();
+            }
+
+            
         }
     }
 }
